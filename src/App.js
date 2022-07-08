147c926da65b011952fa1418e30abfcd8dfe3054
Franco Martelli <fnmartelli@icloud.com>
2022-07-07 23:00:32 -0300

    widget

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './NavBar';
+import ItemListenContainer from './ItemListenContainer';
 
 function App() {
   return (
     <div classNameName="App">
-      <header classNameName="App-header">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Proyect</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav justify-content-end">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Nosotros</a>
-            </li>
-            <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Productos
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-          </ul>
+      <NavBar/>
+        <div>
+          <ItemListenContainer/>
         </div>
-      </div>
-    </nav>
-        <h1>react shop</h1>
-       </header>
     </div>
   );
 }
---
+++ src/ItemListenContainer.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ItemListenContainer = () =>{
+    return (
+        <h1>React js 2.0</h1>
+    );
+}
+
+export default ItemListenContainer;
